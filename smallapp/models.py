d851a8affdebc74e95520ecf83324fa5ef4a7d5c
moringaschool <justusm10@gmail.com>
2021-04-15 02:15:25 +0300

    modify views

--- smallapp/models.py
+++ smallapp/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class List(models.Model):
     title = models.CharField(max_length=200)
 
+    created = models.DateTimeField(auto_now_add=True)
+
     trip = models.CharField(max_length=10)
 
     def __str__(self):
--- smallapp/views.py
+++ smallapp/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import *
+
 # Create your views here.
 
 def index(request):
-    return  render(request, 'smallapp/list.html')
+    
+
+    list = List.objects.all()
+
+    context = { 'list' :list}
+
+
+
+    return  render(request, 'smallapp/list.html', context)
